8c2c0012e1d71b986fe28cb98342d4a92083bf58
“fatyas” <Fatima40679@gmail.com>
2025-11-16 13:04:16 -0800

    updates

--- R/count_all_missing_by_group.r
+++ R/count_all_missing_by_group.r
@@ -90,13 +90,13 @@ testthat::test_that("Output matches direct call to dplyr", {
                      "B", NA, "y",
                      "C", 3, "z")
 
-  expect_equal( small_tbl |> dplyr::group_by(group) |>
+  testthat::expect_equal( small_tbl |> dplyr::group_by(group) |>
                 dplyr::summarize(across(everything(), ~sum(is.na(.x))),
                 .groups = "drop"),
                 count_all_missing_by_group(small_tbl, group)
               )
 
-  expect_equal( small_tbl |> dplyr::group_by(group) |>
+  testthat::expect_equal( small_tbl |> dplyr::group_by(group) |>
                 dplyr::summarize(across(everything(), ~sum(is.na(.x))),
                 .groups = NULL),
                 count_all_missing_by_group(small_tbl, group, NULL)
@@ -104,6 +104,6 @@ testthat::test_that("Output matches direct call to dplyr", {
 })
 
 testthat::test_that("Checking error handling for .groups input", {
-  expect_error(count_all_missing_by_group(airquality, Month, "kep"))
-  expect_no_error(count_all_missing_by_group(airquality, Month, NULL))
+  testthat::expect_error(count_all_missing_by_group(airquality, Month, "kep"))
+  testthat::expect_no_error(count_all_missing_by_group(airquality, Month, NULL))
   })
